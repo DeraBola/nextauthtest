12e82003bb622bb3ed35e66e618b01b934489030
derabola <adu.deraj@gmail.com>
2025-08-10 12:20:22 +0100

    chore: auth setup

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,12 @@ import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import LinkedInProvider from "next-auth/providers/linkedin";
 
-NextAuth({
+export const {
+handlers: {GET, POST},
+auth,
+signIn,
+signOut
+} = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
