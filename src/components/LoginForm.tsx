dfd971d797640bd119687155d064281654858775
derabola <adu.deraj@gmail.com>
2025-08-10 10:33:37 +0100

    created auth test project

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function LoginForm() {
+  return (
+    <form>
+      <button>
+        Sign In with Google
+      </button>
+         <button>
+        Sign In with Google
+      </button>
+    </form>
+  )
+}
+
+export default LoginForm
