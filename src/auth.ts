284c12ae97228ae0e6b3c702eb101da44f04716e
derabola <adu.deraj@gmail.com>
2025-08-11 11:57:02 +0100

    CHore: added github provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-//import GitHubProvider from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 //import LinkedInProvider from "next-auth/providers/linkedin";
 
 export const {
@@ -22,7 +22,18 @@ signOut
         },
       },
     }),
+     GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
   ],
-   secret: process.env.NEXTAUTH_SECRET!,
      debug: true,
 });
