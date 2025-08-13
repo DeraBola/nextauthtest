b469eea3660795098b35d7c69b3fffa9d594387f
derabola <adu.deraj@gmail.com>
2025-08-13 13:10:18 +0100

    updated cognito config

---
+++ src/lib/cognitoConfig.ts
@@ -0,0 +1,7 @@
+export const cognitoAuthConfig = {
+  authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_WrIckx15X", // your pool ID endpoint
+  client_id: "7i1ep58u5qm8vpt3914608vm6f", // your Cognito app client ID
+  redirect_uri: "http://localhost:3000", // must match Cognito app client settings
+  response_type: "code",
+  scope: "email openid phone",
+};
